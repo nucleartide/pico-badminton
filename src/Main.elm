f48bd7ead9a6115ac8b07d658b0969883882223e
Jason Tu <jasontu4@gmail.com>
2018-11-20 07:50:14 -0800

    Add more types

--- src/Main.elm
+++ src/Main.elm
@@ -16,6 +16,14 @@ view   model       = text "hello world"
 type Msg =
   Something
 
+type Button
+  = Left
+  | Right
+  | Up
+  | Down
+  | Z
+  | X
+
 --
 -- Model.
 --
@@ -24,7 +32,9 @@ type Msg =
 --
 
 type alias Model =
-  { something: Int
+  { player         : Player
+  , renderedPlayer : RenderedPlayer
+  , camera         : Camera
   }
 
 type alias Player =
@@ -45,15 +55,73 @@ type alias Ball =
   { position : Vec3
   }
 
+type GameRound
+  = PreServe
+  | Serving
+  | Rally
+  | PostRally
+
 --
 -- Utils.
 --
 
 type alias Vec3 = (Float, Float, Float)
 
-type alias Vec2 = (Float, Float)
+type alias Mat3 = (Vec3, Vec3, Vec3)
 
-type Color
-  = Black
-  | DarkBlue
-  | DarkPurple
+zero : Vec3
+zero = (0, 0, 0)
+
+zeroMat : Mat3
+zeroMat = (zero, zero, zero)
+
+add : Vec3 -> Vec3 -> Vec3
+add _ _ = zero
+
+sub : Vec3 -> Vec3 -> Vec3
+sub _ _ = zero
+
+mul : Vec3 -> Vec3 -> Vec3
+mul _ _ = zero
+
+mulMat3 : Vec3 -> Mat3 -> Vec3
+mulMat3 _ _ = zero
+
+dot : Vec3 -> Vec3 -> Float
+dot _ _ = 0
+
+cross : Vec3 -> Vec3 -> Vec3
+cross _ _ = zero
+
+scale : Vec3 -> Float -> Vec3
+scale _ _ = zero
+
+magnitude : Vec3 -> Float
+magnitude _ = 0
+
+dist : Vec3 -> Vec3 -> Float
+dist _ _ = 0
+
+normalize : Vec3 -> Vec3
+normalize _ = zero
+
+vecLerp : Vec3 -> Vec3 -> Vec3
+vecLerp _ _ = zero
+
+isEqual : Vec3 -> Vec3 -> Bool
+isEqual _ _ = False
+
+rotateX : Float -> Mat3
+rotateX _ = zeroMat
+
+rotateY : Float -> Mat3
+rotateY _ = zeroMat
+
+round : Float -> Float
+round _ = 0
+
+lerp : Float -> Float -> Float -> Float
+lerp _ _ _ = 0
+
+isClockwise : List Vec3 -> Bool
+isClockwise _ = False
